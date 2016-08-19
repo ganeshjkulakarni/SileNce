25df67eed25218ee655e280a4234318d4d163416
Ganesh Kulakarni <engg.ganeshjk@gmail.com>
2016-08-19 11:04:09 +0530

    Project Created

---
+++ app/src/main/java/com/silence/ganesh/silence/Main_Activity.java
@@ -0,0 +1,13 @@
+package com.silence.ganesh.silence;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Main_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_);
+    }
+}
